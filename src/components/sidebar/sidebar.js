16963ab645e2b9ec5bb5f52656955dbe1784f123
Adithyan K S <adithyan.ks@dbizsolution.com>
2025-03-24 14:32:13 +0530

    updated the public folder files

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -11,7 +11,7 @@ React.js. Proficient in HTML, CSS, and JavaScript, with a strong focus on design
 implementing user interfaces, optimizing web performance. Experienced in basic site building
 using Drupal, collaborating with cross-functional teams to deliver high-quality web applications.</p>
     <div className="self-pic">
-        <img src = "/image/self.png"></img>
+    <img src={`${process.env.PUBLIC_URL}/image/self.png`} alt="Self" />
     </div>
         </div>
     </div>
--- src/components/video/video.js
+++ src/components/video/video.js
@@ -4,7 +4,7 @@ function Video() {
   return (
     <div className="video-container">
       <video autoPlay loop muted className="header-video">
-        <source src="/video/homevideo.mp4" type="video/mp4" />
+      <source src={`${process.env.PUBLIC_URL}/video/homevideo.mp4`} type="video/mp4" />
       </video>
     </div>
   );
