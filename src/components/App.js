1b2b08980a020ce11b2feb24d22ee2f55d9254e9
Adithyan K S <adithyan.ks@dbizsolution.com>
2025-03-24 13:14:03 +0530

    app js updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Header from './header/header';
 import Home from './home/home';
 import Video from './video/video';
@@ -9,13 +9,13 @@ function App() {
   return (
     <Router>
       <Video/>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/about" element={<h1 className="text-center mt-10">About Page</h1>} />
-      <Route path="/contact" element={<h1 className="text-center mt-10">Contact Page</h1>} />
-    </Routes>
-  </Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<h1 className="text-center mt-10">About Page</h1>} />
+        <Route path="/contact" element={<h1 className="text-center mt-10">Contact Page</h1>} />
+      </Routes>
+    </Router>
   );
 }
 
